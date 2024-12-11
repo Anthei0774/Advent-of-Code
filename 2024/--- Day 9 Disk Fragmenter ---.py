6cb30c023003a7660354f46057ec37d4d9eb0323
Marcell David Toth <marcelldavid96@gmail.com>
2024-12-11 17:15:33 +0100

    2024/9/2 reworked as well

--- 2024/--- Day 9 Disk Fragmenter ---.py
+++ 2024/--- Day 9 Disk Fragmenter ---.py
@@ -25,30 +25,30 @@ layout = layout_orig.copy()
 i = 0
 while True:
 
+    # find first free space. If no gap, step out
     while (i < len(layout)) and (type(layout[i]) != int):
         i += 1
-
     if i == len(layout):
         break
-
     free_space = layout[i]
 
-    assert type(layout[-1]) == tuple
-
+    # if free space is just enough, then pop last and inject into free space pos
     if layout[-1][1] == free_space:
-        layout[i] = layout[-1]
-        layout = layout[:-1]
+        layout[i] = layout.pop(-1)
+    # elif free space is larger, then decrement space, still popping
     elif layout[-1][1] < free_space:
         layout[i] -= layout[-1][1]
-        layout.insert(i, layout[-1])
-        layout = layout[:-1]
+        layout.insert(i, layout.pop(-1))
+    # else free space is not enough, then fill it, and decrement file size
     else:
         layout[i] = (layout[-1][0], free_space)
         layout[-1] = (layout[-1][0], layout[-1][1] - free_space)
 
+    # if free space is last, delete it
     if type(layout[-1]) != tuple:
         layout = layout[:-1]
 
+# checksum
 S = 0
 m = 0
 for block in layout:
@@ -62,51 +62,48 @@ print("Checksum:", S)
 # PART 2
 
 layout = layout_orig.copy()
-l = len(layout)
 
-id = layout[-1]
-j = layout.index(id)
-
+j = len(layout) - 1
 while True:
 
-    need_space = 1
-    while (j + need_space < l) and (layout[j] == layout[j + need_space]):
-        need_space += 1
-    # print("Need:", layout[j: j + need_space])
-
-    i = layout.index("")
-    while True:
-
-        free_space = 0
-        while (i + free_space < l) and (layout[i] == layout[i + free_space]):
-            free_space += 1
-        # print("Free:", free_space)
-
-        if need_space <= free_space:
-            for k in range(need_space):
-                layout[i + k] = layout[j + k]
-                layout[j + k] = ""
-            break
-        else:
-            if "" in layout[i + free_space:]:
-                i = layout.index("", i + free_space)
-
-                if j < i:
-                    # print("Cannot move block")
-                    break
-            else:
-                break
-
-    id -= 1
-    if id == -1:
+    # search until a move candidate found
+    # exit defragmentation if we pass through the layout
+    while (0 <= j) and (type(layout[j]) != tuple):
+        j -= 1
+    if j == -1:
         break
+
+    # start searching for free space, it cannot occur after j
+    # if we reach j, then current file = layout[j] cannot be moved
+    i = 0
+    while (i < j) and ((type(layout[i]) != int) or (layout[i] < layout[j][1])):
+        i += 1
+    if i == j:
+        j -= 1
+        continue
+
+    # free space is just enough, swap
+    if layout[i] == layout[j][1]:
+        tmp = layout[i]
+        layout[i] = layout[j]
+        layout[j] = tmp
+    # free space is larger, fill and decrement like before
     else:
-        j = layout.index(id)
+        layout[i] -= layout[j][1]
+        tmp = layout[j]
+        layout[j] = layout[j][1]
+        layout.insert(i, tmp)
 
-        if "" not in layout[:j]:
-            break
+    # in this last if-else was encountered, then no need to decrement j again. On its place, and integer should be by the swap, or it is already shifted by the insert
 
-        print(j)
+# checksum
+S = 0
+m = 0
+for block in layout:
+    if type(block) == int:
+        block = (0, block)
+    for i in range(block[1]):
+        S += (m * block[0])
+        m += 1
 
-layout = [value if value != "" else 0 for value in layout]
-print("Checksum:", sum(i * layout[i] for i in range(l)))
+print("Checksum:", S)
