5e54fb58dca39ab5800849b8a760a5d5594d9ba3
Marcell David Toth <marcelldavid96@gmail.com>
2024-12-13 11:08:27 +0100

    2024/6, half a min speed up on part 2, runs in 1:30

--- 2024/--- Day 6 Guard Gallivant ---.py
+++ 2024/--- Day 6 Guard Gallivant ---.py
@@ -45,14 +45,8 @@ del map
 pos = start_pos
 dir = (-1, 0)
 
-# memorization for part 2
-original_path = [(pos, dir)]
-unique_locations = {pos: 0}
-
-i = 1
+unique_locations = [start_pos]
 while True:
-
-    # append new location with direction
 
     next = (pos[0] + dir[0], pos[1] + dir[1])
     if (0 <= next[0] and next[0] < R) and (0 <= next[1] and next[1] < C):
@@ -63,15 +57,10 @@ while True:
         else:
             pos = next
             if pos not in unique_locations:
-                unique_locations[pos] = i
-
-        original_path.append((pos, dir))
-        i += 1
-
+                unique_locations.append(pos)
     else:
         # guard moves off the map, ending patrol
         break
-
 
 print("Number of unique locations:", len(unique_locations))
 
@@ -79,19 +68,18 @@ print("Number of unique locations:", len(unique_locations))
 # PART 2
 
 # idea I: put obstruction to pos only if the guard visited, because it makes no sense to check for a location which was not encountered in the original path
-del unique_locations[start_pos]
-
-# idea II: since we stored the original path, we can fast forward the guard's route to the point where he would hit the obstruction ==> but we need to store the indices of positions first stepped on, hence the memorization in previous part
+unique_locations.remove(start_pos)
 
 good_locations = []
 for loc in tqdm(unique_locations):
 
     obstacles.append(loc)
 
-    i = unique_locations[loc]
-    new_path = original_path[:i]
-    pos = new_path[-1][0]
-    dir = new_path[-1][1]
+    pos = start_pos
+    dir = (-1, 0)
+
+    # idea II: log only the turns, so no need to check every step of the new path
+    turns = []
 
     while True:
 
@@ -101,15 +89,15 @@ for loc in tqdm(unique_locations):
             # next step is on the map, check if need to turn
             if next in obstacles:
                 dir = (dir[1], -dir[0])
+
+                # we started looping, break
+                if (pos, dir) in turns:
+                    good_locations.append(loc)
+                    break
+                else:
+                    turns.append((pos, dir))
             else:
                 pos = next
-
-            if (pos, dir) in new_path:
-                good_locations.append(loc)
-                # guard entered loop with obstacle at loc, reset
-                break
-            else:
-                new_path.append((pos, dir))
 
         else:
             # guard moves off the map, ending patrol
